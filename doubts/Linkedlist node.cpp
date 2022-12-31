7c84aabe81dde4c3f8176cb72428551f5f9e394d
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2022-12-31 09:32:37 +0530

    Create Linkedlist node.cpp

---
+++ doubts/Linkedlist node.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+//we have created a structure 
+struct node //  structure  name is node 
+{ 
+int data;. // which contains data of the node 
+struct node *next;  // *next means it is a pointer having the address of the next node
+};
+struct node *head = NULL; //we have given the value in head = 0
+int insertionofnode( int new_data) // now I have created a function naming insertion of node
+   // jisme we will have a data which will be inserted in the beginning of the linkedlist
+struct node *node1 = new node(); //
+
+
+
+{
+
+}
